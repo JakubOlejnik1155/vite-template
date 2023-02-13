6aad1544f01ca80f44a31a79624252857d2e072e
JakubOlejnik1155 <jakub.k.olejnik@gmail.com>
2023-02-13 23:14:04 +0100

    fix base for images

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { resolve } from 'path';
 import handlebars from 'vite-plugin-handlebars';
 
 export default {
-  plugins: [handlebars(
+    base: '',
+    plugins: [handlebars(
     {
       partialDirectory: resolve(__dirname, 'partials'),
     }
